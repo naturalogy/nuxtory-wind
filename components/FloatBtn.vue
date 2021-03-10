b6abae5e0adcb5371a3bd3b693f6a2290f43871d
naturalogy <naturalogy@users.noreply.github.com>
2021-03-10 13:42:51 +0900

    Add example of floating button

---
+++ components/FloatBtn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fixed right-3 bottom-3">
+    <button class="btn btn-primary" @click="reset()">
+      <i class="fas fa-sync-alt"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    reset: () => location.reload(),
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  @apply leading-none p-6 rounded-full shadow-md;
+}
+</style>
